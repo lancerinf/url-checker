60231a44e1b8a3d8e0d029355b5c1d30fd65f13c
Federico Lancerin <federico.lancerin@gmail.com>
2021-08-03 13:28:19 +0200

    bug-fix: update CACHE when diff is found

--- src/main/main.py
+++ src/main/main.py
@@ -41,6 +41,7 @@ def check_url(page_url):
             diff_result = list(context_diff(reference, links_in_text_of_interest))
             if diff_result:
                 log_message(f"Some links have changed! Check: {page_url}")
+                LINKS_CACHE[page_url] = links_in_text_of_interest
                 send_email_alert(page_url, diff_result)
             else:
                 log_message("Links haven't changed since last visit.")
@@ -56,14 +57,15 @@ def send_email_alert(page_url, diff):
     sender = EMAIL_SENDER
     recipient = EMAIL_RECIPIENT
     subject = "Url Checker: Some links have changed!"
-    body_text = ("Url Checker checkin-in!\r\n"
-                 "\r\n"
-                 f"Links have changed in: {page_url}\r\n"
-                 "\r\n"
-                 "Diff:\r\n"
-                 f"{diff}\r\n"
-                 )
     charset = "UTF-8"
+    body_text = (
+        "Url Checker checkin-in!\r\n"
+        "\r\n"
+        f"Links have changed in: {page_url}\r\n"
+        "\r\n"
+        "Diff:\r\n"
+        '\r\n'.join(diff)
+    )
 
     aws_session = boto3.session.Session(profile_name=EMAIL_THROUGH_AWS_PROFILE)
     client = aws_session.client('ses', region_name="eu-west-1")
